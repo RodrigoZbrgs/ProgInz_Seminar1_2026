1ab4830eb92319af8eaec4a9cc256da626b87c44
RodrigoZbrgs <rodrigo.zandbergs@gmail.com>
2026-05-15 19:14:01 +0300

    New stuff

---
+++ src/main/java/lv/venta/service/IProductFillerAndStatsService.java
@@ -0,0 +1,10 @@
+package lv.venta.service;
+
+public interface IProductFillerAndStatsService {
+	//TODO abstraktas funkcijas
+	// filtret produktus pec cenas, kas mazaka par ienakoso mainiga vertibu
+	//filtret produktus pec tipa
+	//filtret produktus pec atslegas varda, kas varetu but title vai ari description
+	
+	//aprekinat videjo vertibu produktu cenam
+}
